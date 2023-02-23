787fc70fcf011ee8f731e0618323daa6057525c2
leandro4silva <leandro94.a.s@gmail.com>
2023-02-22 22:34:52 -0300

    fix: retorno da rota days

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -65,13 +65,13 @@ export async function appRoutes(app: FastifyInstance) {
             }
         })
 
-        const compledHabits = day?.dayHabits.map(dayHabit => {
+        const completedHabits = day?.dayHabits.map(dayHabit => {
             return dayHabit.habit_id
-        })
+        }) ?? [];
 
         return {
             possibleHabits,
-            compledHabits
+            completedHabits
         }
     });
 
